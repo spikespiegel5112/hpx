a65f7b03290b8e41c235bd13d9153babc4352451
spikespiegel5112 <583288277@qq.com>
2017-07-31 18:07:43 +0800

    temp

---
+++ vue-app/src/page/register.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="register">
+        <div class="register-container">
+            <h1>用户注册</h1>
+        </div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+.register {
+    background-image: url("../assets/img/background.png");
+    background-size: 100% 100%;
+    width: 100%;
+    height: 100%;
+}
+
+.register-container {
+    width: 30%;
+    height: 600px;
+    margin-top: 50px;
+    margin: 0 auto;
+}
+</style>
+
