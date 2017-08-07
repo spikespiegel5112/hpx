96c3f5c4dfb8de82a03aa103bd9230dafa9e7df7
spikespiegel5112 <583288277@qq.com>
2017-08-07 19:53:24 +0800

    temp

---
+++ vue-app/src/components/commonDetailTitle.vue
@@ -0,0 +1,14 @@
+<template>
+<div class='common_detailtitle_item'>
+    <router-link v-if="routerName" :to="{name:routerName, params:param} ">
+        <span class='el-icon-arrow-left'></span>
+        <label>返回</label>
+    </router-link>
+    <h3>{{title}}</h3>
+</div>
+</template>
+<script>
+export default {
+    props: ['title', 'routerName', 'param']
+}
+</script>
