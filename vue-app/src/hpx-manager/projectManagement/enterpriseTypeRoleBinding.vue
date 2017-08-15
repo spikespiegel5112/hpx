f0ad1287d39d764a84fdab1aaf019cac5e1ab78d
spikespiegel5112 <583288277@qq.com>
2017-08-15 10:35:28 +0800

    temp

--- vue-app/src/hpx-manager/projectManagement/enterpriseTypeRoleBinding.vue
+++ vue-app/src/hpx-manager/projectManagement/enterpriseTypeRoleBinding.vue
@@ -193,6 +193,7 @@ export default {
 			this.getUnbindedRolesList();
 			this.bindEnterpriseTypeFlag = true;
 		},
+        
         bindEnterpriseTypeSubmit() {
             this.$refs['bindRoleData'].validate((valid) => {
 //                if(valid){
